d86b3228cd06b24d3a6deaf1315893682197bb8c
mkewls <mw@mikewill.net>
2016-09-19 12:55:04 -0400

    updated echo cat head tail

--- bash.js
+++ bash.js
@@ -5,16 +5,18 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-
+  // receive space-delimited string
+  var dataArr = data.toString().trim().split(' ');
   // Handle user input through command methods
-  command.userIn(cmd);
+  var cmd = dataArr[0].toString();
+  var filename = dataArr.slice(1).join(' ');
+  command.userIn(cmd, filename);
 
   // next prompt
   var timeOut = setTimeout(function() {
   	process.stdout.write('\nprompt > ');
   }, 0);
-  if(cmd === 'ls') {
+  if(cmd === 'ls' || cmd === 'cat' || cmd === 'head' || cmd === 'tail') {
   	clearTimeout(timeOut);
   }
 
--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
+var path = require('path');
  // PWD, Date, and Exit
-exports.userIn = function(cmd) {
+exports.userIn = function(cmd, filename) {
   if (cmd === 'pwd') {
     return process.stdout.write(process.cwd());
   }
@@ -21,7 +22,33 @@ exports.userIn = function(cmd) {
       })
     process.stdout.write('prompt > ');
     });
-    //clearTimeout(timeOut);
+  }
+  if (cmd === 'echo') {
+    if (filename === '$PATH') {
+      process.stdout.write(process.env.PATH)
+    } else {
+      process.stdout.write(args);
+    }
+  }
+  if (cmd === 'cat') {
+    fs.readFile(filename, (err, data) => {
+      if (err) { throw err; };
+      process.stdout.write(data);
+      process.stdout.write('prompt > ');
+    });
+  }
+  if (cmd === 'head') {
+    fs.readFile(filename, (err, data) => {
+      if (err) { throw err; };
+      var objKeys = Object.keys(data);
+      var objArr = [];
+      for (key of objKeys) {
+        objArr.push(data[key]);
+      }
+      var firstTwo = objArr.slice(0, 2).join(' ');
+      process.stdout.write(firstTwo);
+      process.stdout.write('prompt > ');
+    })
   }
 
 };
